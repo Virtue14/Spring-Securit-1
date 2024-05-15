c5617b1d8bc887c60dacb82c199954a4d30fab8c
악덕 <rudejrqkr97@gmail.com>
2024-05-16 03:09:14 +0900

    feat: Admin 페이지 추가 및 SecurityConfig 파일 설정
    - 새로운 admin.mustache 템플릿 파일 추가
    - AdminController.java에 Admin 페이지 컨트롤러 추가
    - SecurityConfig.java에 Admin 페이지 접근 권한 설정 추가

---
+++ src/main/java/com/virtue/spring/security/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.virtue.spring.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/", "login").permitAll()
+                        .requestMatchers("/admin").hasAnyRole("ADMIN")
+                        .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER")
+                        .anyRequest().authenticated()   // 로그인 한 사용자만 허용
+                );
+
+        return http.build();
+
+    }
+
+}
