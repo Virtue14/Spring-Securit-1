856dd367619c0f6c4a1a10ae85b6e8f7e1b4c55d
악덕 <rudejrqkr97@gmail.com>
2024-05-17 12:15:16 +0900

    feat: 새로운 세션 관리 기능을 추가했습니다.
    - 세션 타임아웃을 설정할 수 있게 되었습니다.
    - 로그인 시 세션 정보 변경 여부를 설정할 수 있습니다.
    - 최대 세션 수 및 초과 시 처리 방식을 설정할 수 있습니다.

--- src/main/java/com/virtue/spring/security/config/SecurityConfig.java
+++ src/main/java/com/virtue/spring/security/config/SecurityConfig.java
@@ -14,6 +14,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
+        // 경로 인가
         http
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/", "login", "loginProc", "join", "joinProc").permitAll()
@@ -22,14 +23,32 @@ public class SecurityConfig {
                         .anyRequest().authenticated()   // 로그인 한 사용자만 허용
                 );
 
+        // 로그인 폼
         http
                 .formLogin((auth) -> auth.loginPage("/login")
                         .loginProcessingUrl("/loginProc")
                         .permitAll()
                 );
 
+        // csrf
         http
                 .csrf((auth) -> auth.disable());
+
+        http
+                .sessionManagement((auth) -> auth
+                        .maximumSessions(1)
+                        .maxSessionsPreventsLogin(true));
+
+        // true : 초과 시 새로운 로그인 차단
+        // false : 초과시 기존 세션 하나 삭제
+
+        http
+                .sessionManagement((auth) -> auth
+                        .sessionFixation().changeSessionId());
+        // 설명: 세션 고정보호
+        // none : 로그인 시 세션 정보 변경 안함
+        // newSession : 로그인 시 세션 새로 생성
+        // changeSessionId : 로그인 시 동일한 세션에 대한 id 변경 -> 주로 쓰임
 
         return http.build();
 
